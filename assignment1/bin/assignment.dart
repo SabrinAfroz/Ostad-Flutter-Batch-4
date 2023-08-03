8498e63db249513856afd8de37db5e23639481c3
SabrinAfroz <54852617+SabrinAfroz@users.noreply.github.com>
2023-08-03 16:53:34 +0600

    Add files via upload

---
+++ assignment1/bin/assignment.dart
@@ -0,0 +1,16 @@
+import 'package:assignment/assignment.dart' as assignment;
+
+void main() {
+  const int a = 7;
+  const int b = 3;
+
+  int add = a+b;
+  int sub = a-b;
+  int multi = a*b;
+  double div = a/b;
+  int mod = a%b;
+
+  print('Addition:$a+$b =  $add\nSubtraction:$a-$b =  $sub\nMultiplication:$a*$b =  $multi\nDivision:$a/$b = $div\nModulation:$a%$b =  $mod');
+
+
+}
